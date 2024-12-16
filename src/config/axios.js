08fe02e015903080dd426ec9d50dd2b1aef711e9
afianhyira <nhyiraokla@gmail.com>
2024-12-16 20:50:06 +0000

    change baseURL

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // baseURL: "http://localhost:5001/api",
+  baseURL: "https://campus-event-backbend.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
